b5d53b683a7035eef681aee28cf64cc29394ee40
canna <canna>
2002-05-16 03:09:37 +0000

    login... aww yeah! ( - kerberos :)

--- daemon/command.c
+++ daemon/command.c
@@ -9,6 +9,7 @@
 #include <sys/param.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <string.h>
@@ -23,6 +24,8 @@
 
 #include "command.h"
 
+#define CPATH "/var/cosign"
+
 extern char	*version;
 
 struct command {
@@ -33,6 +36,7 @@ struct command {
 static int	f_noop ___P(( SNET *, int, char *[] ));
 static int	f_quit ___P(( SNET *, int, char *[] ));
 static int	f_help ___P(( SNET *, int, char *[] ));
+static int	f_login ___P(( SNET *, int, char *[] ));
 
 
     int
@@ -65,6 +69,98 @@ f_help( sn, ac, av )
     snet_writef( sn, "%d Vaild commands are HELP, NOOP, and QUIT\r\n", 203 );
     return( 0 );
 }
+    int
+f_login( sn, ac, av )
+    SNET        *sn;
+    int         ac;
+    char        *av[];
+{
+    char		path[ MAXPATHLEN ], tmppath[ MAXPATHLEN ];
+    FILE		*tmpfile;
+    struct timeval	tv;
+    int			fd;
+    extern int		errno;
+
+    /* login login_cookie ip principal realm [tgt] */
+
+    if ( ac != 5 ) {
+	snet_writef( sn, "%d LOGIN Syntax error\r\n", 500 );
+	return( 1 );
+    }
+
+    if ( snprintf( path, MAXPATHLEN, "%s/%s", CPATH, av[ 1 ] ) >= MAXPATHLEN ) {
+	snet_writef( sn, "%d LOGIN Syntax error: Cookie too long\r\n", 501 );
+	return( 1 );
+    }
+
+    if ( gettimeofday( &tv, NULL ) != 0 ){
+	syslog( LOG_ERR, "f_login: gettimeofday: %m" );
+	snet_writef( sn, "%d LOGIN Fatal Error: Sorry!\r\n", 502 );
+	return( -1 );
+    }
+
+    if ( snprintf( tmppath, MAXPATHLEN, "%s/%x%x.%i", CPATH,
+	    tv.tv_sec, tv.tv_usec, (int)getpid()) >= MAXPATHLEN ) {
+	syslog( LOG_ERR, "f_login: tmppath too long" );
+	snet_writef( sn, "%d LOGIN Fatal Error: Sorry!\r\n", 502 );
+	return( -1 );
+    }
+
+    if (( fd = open( tmppath, O_CREAT|O_EXCL|O_WRONLY, 0644 )) < 0 ) {
+	syslog( LOG_ERR, "f_login: open: %m" );
+	snet_writef( sn, "%d LOGIN Fatal Error: Sorry!\r\n", 502 );
+	return( -1 );
+    }
+
+    if (( tmpfile = fdopen( fd, "w" )) == NULL ) {
+	/* close */
+	if ( unlink( tmppath ) != 0 ) {
+	    syslog( LOG_ERR, "f_login: unlink: %m" );
+	}
+	syslog( LOG_ERR, "f_login: fdopen: %m" );
+	snet_writef( sn, "%d LOGIN Fatal Error: Sorry!\r\n", 502 );
+	return( -1 );
+    }
+
+    syslog( LOG_INFO, "f_login: tmpfile: %s", tmppath );
+
+    fprintf( tmpfile, "v0\n" );
+    fprintf( tmpfile, "i%s\n", av[ 2 ] );
+    fprintf( tmpfile, "p%s\n", av[ 3 ] );
+    fprintf( tmpfile, "r%s\n", av[ 4 ] );
+    fprintf( tmpfile, "t%lu\n", tv.tv_sec );
+    fprintf( tmpfile, "s1\n" );	 /* 1 is logged in, 0 is logged out */
+
+    if ( fclose ( tmpfile ) != 0 ) {
+	if ( unlink( tmppath ) != 0 ) {
+	    syslog( LOG_ERR, "f_login: unlink: %m" );
+	}
+	syslog( LOG_ERR, "f_login: fclose: %m" );
+	snet_writef( sn, "%d LOGIN Fatal Error: Sorry!\r\n", 502 );
+	return( -1 );
+    }
+
+    if ( link( tmppath, path ) != 0 ) {
+	if ( unlink( tmppath ) != 0 ) {
+	    syslog( LOG_ERR, "f_login: unlink: %m" );
+	}
+	if( errno == EEXIST ) {
+	    syslog( LOG_ERR, "f_login: file already exists: %s", path );
+	    snet_writef( sn,
+		    "%d LOGIN error: Cookie already exists\r\n", 400 );
+	    return( 1 );
+	}
+	syslog( LOG_ERR, "f_login: link: %m" );
+	snet_writef( sn, "%d LOGIN Fatal Error: Sorry!\r\n", 502 );
+	return( -1 );
+    }
+
+    if ( unlink( tmppath ) != 0 ) {
+	syslog( LOG_ERR, "f_login: unlink: %m" );
+    }
+    snet_writef( sn, "%d LOGIN successful: Cookie Stored \r\n", 200 );
+    return( 0 );
+}
 
 
 
@@ -72,6 +168,7 @@ struct command	commands[] = {
     { "NOOP",		f_noop },
     { "QUIT",		f_quit },
     { "HELP",		f_help },
+    { "LOGIN",		f_login },
 };
 int		ncommands = sizeof( commands ) / sizeof( commands[ 0 ] );
 
