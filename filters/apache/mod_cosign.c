ec0b5764b9289a6d2c149df537e955a3386b4712
canna <canna>
2002-06-11 19:26:12 +0000

    clean env so cgis don't see cookie. need directive for such?

--- filters/apache/mod_cosign.c
+++ filters/apache/mod_cosign.c
@@ -150,13 +150,13 @@ validate_cookie:
     strcpy( si.si_ipaddr, r->connection->remote_ip );
     if ( cookie_valid( cfg->sl, my_cookie, &si ) < 0 ) {
 fprintf( stderr, "want to redirect now!\n" );
-    #ifdef notdef
+#ifdef notdef
 	if ( set_cookie_and_redirect( r, cfg ) == 0 ) {
 	    return( HTTP_MOVED_TEMPORARILY );
 	} else {
 	    return( FORBIDDEN );
 	}
-    #endif notdef
+#endif notdef
     }
     ap_table_set( r->subprocess_env, "REMOTE_REALM", si.si_realm );
     r->connection->user = ap_pstrcat( r->pool, si.si_user, NULL);
@@ -164,6 +164,15 @@ fprintf( stderr, "want to redirect now!\n" );
 
     return( DECLINED );
 }
+
+    int
+cosign_fixup( request_rec *r )
+{
+    /* add a directive and figure out the cfg stuff */
+    ap_table_unset( r->headers_in, "Cookie" );
+    return( DECLINED );
+}
+
 
     static const char *
 set_cosign_protect( cmd_parms *params, void *mconfig, int flag )
@@ -370,7 +379,7 @@ module MODULE_VAR_EXPORT cosign_module = {
     NULL,                  /* [#5] check if the user is ok _here_ */
     cosign_auth,           /* [#3] check access by host address   */
     NULL,                  /* [#6] determine MIME type            */
-    NULL,                  /* [#7] pre-run fixups                 */
+    cosign_fixup,	   /* [#7] pre-run fixups                 */
     NULL,                  /* [#9] log a transaction              */
     NULL,                  /* [#2] header parser                  */
     NULL,                  /* child_init                          */
