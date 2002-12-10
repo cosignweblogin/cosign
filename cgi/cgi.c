508f78ab727d68d2c61e2353314c2ccb5fce9990
canna <canna>
2002-12-10 20:01:54 +0000

    stores tickets now.

--- cgi/cgi.c
+++ cgi/cgi.c
@@ -22,13 +22,14 @@
 #define REDIRECT_HTML	"../templates/redirect.html"
 #define SERVICE_MENU	"../templates/service-menu.html"
 #define SPLASH_HTML	"../templates/splash.html"
+#define TKT_PREFIX	"/ticket/"
 #define SIDEWAYS        1
 
 extern char	*version;
 char	*err = NULL;
 char	*url = "http://www.umich.edu/";
 char	*title = "Authentication Required";
-char	*host = "weblogin.umich.edu";
+char	*host = "cosign-test.www.umich.edu";
 int	nocache = 0;
 int	port = 6663;
 
@@ -132,9 +133,11 @@ main( int argc, char *argv[] )
     krb5_principal		kprinc;
     krb5_get_init_creds_opt	kopts;
     krb5_creds			kcreds;
+    krb5_ccache			kccache;
     int				rc;
     char                	new_cookiebuf[ 128 ];
     char        		new_cookie[ 255 ];
+    char               		tmpkrb[ 16 ], krbpath [ 24 ];
     char			*data, *ip_addr, *service, *ref = NULL;
     char			*cookie = NULL, *method, *script, *qs;
     char			*tmpl = LOGIN_HTML;
@@ -279,8 +282,28 @@ main( int argc, char *argv[] )
 	exit( 0 );
     }
 
+
     if (( kerror = krb5_parse_name( kcontext, cl[ CL_UNIQNAME ].cl_data,
 	    &kprinc ))) {
+	err = (char *)error_message( kerror );
+	title = "Authentication Required ( kerberos error )";
+
+	tmpl = ERROR_HTML;
+	subfile ( tmpl );
+	exit( 0 );
+    }
+
+    if ( mkcookie( sizeof( tmpkrb ), tmpkrb ) != 0 ) {
+	err = "An unknown error occurred.";
+	title = "Authentication Required ( kerberos error )";
+
+	tmpl = ERROR_HTML;
+	subfile ( tmpl );
+	exit( 0 );
+    }
+    sprintf( krbpath, "%s/%s", TKT_PREFIX, tmpkrb );
+
+    if (( krb5_cc_resolve( kcontext, krbpath, &kccache )) != 0 ) {
 	err = (char *)error_message( kerror );
 	title = "Authentication Required ( kerberos error )";
 
@@ -292,28 +315,55 @@ main( int argc, char *argv[] )
     krb5_get_init_creds_opt_init( &kopts );
     krb5_get_init_creds_opt_set_tkt_life( &kopts, 5*60 );
     krb5_get_init_creds_opt_set_renew_life( &kopts, 0 );
-    krb5_get_init_creds_opt_set_forwardable( &kopts, 0 );
+    krb5_get_init_creds_opt_set_forwardable( &kopts, 1 );
     krb5_get_init_creds_opt_set_proxiable( &kopts, 0 );
+    krb5_get_init_creds_opt_set_address_list( &kopts, NULL );
 
     if (( kerror = krb5_get_init_creds_password( kcontext, &kcreds, 
 	    kprinc, cl[ CL_PASSWORD ].cl_data, krb5_prompter_posix, NULL, 0, 
-	    "kadmin/changepw", &kopts ))) {
+	    NULL /*keytab */, &kopts ))) {
 
 	if ( kerror == KRB5KRB_AP_ERR_BAD_INTEGRITY ) {
 
 	    err = "Password incorrect.  Is [caps lock] on?";
 	    title = "Authentication Required ( Password Incorrect )";
 
+	    tmpl = ERROR_HTML;
 	    subfile ( tmpl );
 	    exit( 0 );
 	} else {
 	    err = (char *)error_message( kerror );
 	    title = "( Password Error )";
 	    
+	    tmpl = ERROR_HTML;
 	    subfile ( tmpl );
 	    exit( 0 );
 	}
     }
+
+    if (( krb5_cc_initialize( kcontext, kccache, kprinc )) != 0 ) {
+	err = (char *)error_message( kerror );
+	title = "( Ticket Sticking Error )";
+	
+	tmpl = ERROR_HTML;
+	subfile ( tmpl );
+	exit( 0 );
+    }
+
+    if (( krb5_cc_store_cred( kcontext, kccache, &kcreds )) != 0 ) {
+	err = (char *)error_message( kerror );
+	title = "( Ticket Storing Error )";
+	
+	tmpl = ERROR_HTML;
+	subfile ( tmpl );
+	exit( 0 );
+    }
+
+    krb5_free_cred_contents( kcontext, &kcreds );
+    krb5_free_principal( kcontext, kprinc );
+    krb5_cc_close( kcontext, kccache );
+    krb5_free_context( kcontext );
+    /* have name and give that to login */
 
     /* password has been accepted, tell cosignd */
     err = "Your password has been accepted.";
