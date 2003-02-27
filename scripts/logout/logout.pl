999d6d294a3702505b447f8120b69806e3d532bb
clunis <clunis>
2003-02-27 18:10:40 +0000

    logout now uses new COSIGN_SERVICE env variable.

--- scripts/logout/logout.pl
+++ scripts/logout/logout.pl
@@ -5,11 +5,8 @@ use strict;
 # change 'central' to the url of your central weblogin server.
 my $central = "https://weblogin.umich.edu/cgi-bin/logout";
 
-# change 'service' to your service name.
-my $service = "mail";
-
 # expire and nullify service cookie
-print( "Set-Cookie: cosign-$service=null; path=/; expires=Wednesday, 16-Apr-73 02:10:00 GMT; secure\n" );
+print( "Set-Cookie: $ENV{ COSIGN_SERVICE }=null; path=/; expires=Wednesday, 27-Jan-77 00:00:00 GMT; secure\n" );
 
 # perform any local cleanup here
 
