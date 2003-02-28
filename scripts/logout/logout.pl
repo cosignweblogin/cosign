4df7de014d20cb0978933b838486c5ed448c438a
clunis <clunis>
2003-02-28 14:26:26 +0000

    logout needs to pass quesry string.

--- scripts/logout/logout.pl
+++ scripts/logout/logout.pl
@@ -4,13 +4,18 @@ use strict;
 
 # change 'central' to the url of your central weblogin server.
 my $central = "https://weblogin.umich.edu/cgi-bin/logout";
+my $query_string = "?http://directory.umich.edu/";
 
 # expire and nullify service cookie
-print( "Set-Cookie: $ENV{ COSIGN_SERVICE }=null; path=/; expires=Wednesday, 27-Jan-77 00:00:00 GMT; secure\n" );
+print( "Set-Cookie: cosign-directory=null; path=/; expires=Wednesday, 27-Jan-77 00:00:00 GMT; secure\n" );
+
+if ( $ENV{ QUERY_STRING } =~ m|^(https?://.*)$| ) {
+    $query_string = "?$1";
+}
 
 # perform any local cleanup here
 
 # redirect to central weblogin server
-print( "Location: $central\n\n" );
+print( "Location: $central$query_string\n\n" );
 
 exit( 0 );
