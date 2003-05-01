ed7de3a006c51143e0cf299a36f7f0ac6e7c9b32
clunis <clunis>
2003-05-01 19:30:34 +0000

    perl and python helloCosign

--- scripts/hello/helloCosign.pl
+++ scripts/hello/helloCosign.pl
@@ -4,6 +4,9 @@ use strict;
 
 while( <DATA> ) {
     s/REMOTE_USER/$ENV{ REMOTE_USER }/;
+    s/COSIGN_SERVICE/$ENV{ COSIGN_SERVICE }/;
+    s/REMOTE_REALM/$ENV{ REMOTE_REALM }/;
+    s/AUTH_TYPE/$ENV{ AUTH_TYPE }/;
     print();
 }
 
@@ -21,5 +24,12 @@ Content-type: text/html
         <h1>
             Hello, REMOTE_USER!
         </h1>
+
+	<table>
+	    <tr> <td>Remote User</td> <td>REMOTE_USER</td> </tr>
+	    <tr> <td>Cosign Service</td> <td>COSIGN_SERVICE</td> </tr>
+	    <tr> <td>Remote Realm</td> <td>REMOTE_REALM</td> </tr>
+	    <tr> <td>Auth Type</td> <td>AUTH_TYPE</td> </tr>
+	</table>
     </body>
 </html>
--- scripts/hello/helloCosign.py
+++ scripts/hello/helloCosign.py
@@ -3,8 +3,22 @@
 import os
 
 print "Content-type: text/html\n\n"
-print
-print
 
-print "Hello, ", os.environ['REMOTE_USER'], "!"
+print """
+<html>
+    <head>
+	<title>hello, cosign ( python )</title>
+    </head>
+
+    <body>
+	<h1>
+"""
+
+print "            Hello, ", os.environ['REMOTE_USER']
+
+print """
+!\n        </h1>
+    </body>
+</html>
+"""
 
