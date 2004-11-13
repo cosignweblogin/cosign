433381d7c84c355ae445faf16e39f2a6b2dc376d
wes <wes>
2004-11-13 16:54:37 +0000

    mattbing@umich.edu: RAND_bytes() returns 1 on success, 0 otherwise.

--- common/mkcookie.c
+++ common/mkcookie.c
@@ -16,7 +16,7 @@ mkcookie( int len, char *buf )
 	return( -1 );
     }
 
-    if ( RAND_bytes( tmp, randbytes ) < 0 ) {
+    if ( RAND_bytes( tmp, randbytes ) != 1 ) {
         return( -2 );
     }
 
