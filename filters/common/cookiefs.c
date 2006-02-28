dd1c687f29f84caf6a0a7796f169f28953ab03bb
canna <canna>
2006-02-28 16:37:35 +0000

    added more detailed logging for network/local cahce data mistmatch.

--- filters/common/cookiefs.c
+++ filters/common/cookiefs.c
@@ -143,11 +143,21 @@ netcheck:
 
     if ( rs == 0 ) {
 	/* check net info against local info */
-	if (( strcmp( si->si_ipaddr, lsi.si_ipaddr ) != 0 ) ||
-		( strcmp( si->si_user, lsi.si_user ) != 0 ) ||
-		( strcmp( si->si_realm, lsi.si_realm ) != 0 )) {
+	if ( strcmp( si->si_ipaddr, lsi.si_ipaddr ) != 0 ) {
 	    cosign_log( APLOG_ERR, s, "mod_cosign: cosign_cookie_valid: "
-		    "network info does not match local info for %s", cookie );
+		    "network info %s does not match local info %s for "
+		    "cookie %s", si->si_ipaddr, lsi.si_ipaddr, cookie );
+	    return( COSIGN_ERROR );
+	}
+	if ( strcmp( si->si_user, lsi.si_user ) != 0 ) {
+	    cosign_log( APLOG_ERR, s, "mod_cosign: cosign_cookie_valid: "
+		    "network info %s does not match local info %s for "
+		    "cookie %s", si->si_user, lsi.si_user, cookie );
+	}
+	if ( strcmp( si->si_realm, lsi.si_realm ) != 0 ) {
+	    cosign_log( APLOG_ERR, s, "mod_cosign: cosign_cookie_valid: "
+		    "network info %s does not match local info %s for "
+		    "cookie %s", si->si_realm, lsi.si_realm, cookie );
 	    return( COSIGN_ERROR );
 	}
 
