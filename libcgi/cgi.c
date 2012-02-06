143224269255c7a098c026765293f517c46188a2
Andrew Mortensen <admorten@umich.edu>
2012-02-06 10:55:10 -0500

    Fix: cgi_init malloc failure can lead to NULL dereference.
    CGI_SYSERR and CGI_LOGERR dereference cgi. If malloc fails, cgi will be
    NULL. Found with clang --analyze.

--- libcgi/cgi.c
+++ libcgi/cgi.c
@@ -129,7 +129,8 @@ cgi_init( void )
 
 DEBUG( fprintf( stderr, "DB: in cgi_init\n" ));
     if (( cgi = (CGIHANDLE *)malloc( sizeof( CGIHANDLE ))) == NULL ) {
-	goto error;
+	perror( "cgi_init: malloc CGIHANDLE" );
+	return( NULL );
     }
     memset( cgi, 0, sizeof( CGIHANDLE ));
 
